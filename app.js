6f91f13a928d70ccaaea80a75f4fc9bd6d6fe765
fabigazi <fgaziano98@gmail.com>
2023-06-22 15:52:19 -0400

    all hooks should be ready

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import session from "express-session";
 import mongoose from "mongoose";
 
 mongoose.connect(
-  "mongodb+srv://jannunzi:M3z9JRSvzhbUdSiu@cluster0.erbnm4p.mongodb.net/tuiter-su1-23?retryWrites=true&w=majority"
+  "mongodb+srv://Guest:Guest@cluster0.kcmgz9i.mongodb.net/?retryWrites=true&w=majority"
 );
 // mongoose.connect("mongodb://127.0.0.1:27017/tuiter-su1-23");
 
@@ -17,7 +17,7 @@ app.set("trust proxy", 1);
 app.use(
   cors({
     credentials: true,
-    origin: "https://a6--resonant-quokka-5a61c8.netlify.app",
+    origin: "https://main--warm-cendol-deab82.netlify.app",
     // origin: "http://localhost:3000",
   })
 );
