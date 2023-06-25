54142af0fb01a44005eaaf80883e4504bcf91032
businessmajor <barrios.st@northeastern.edu>
2023-06-25 16:47:24 -0400

    readded header thing

--- app.js
+++ app.js
@@ -27,15 +27,15 @@ app.use(
   })
 );
 
-// app.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', ["https://main--warm-cendol-deab82.netlify.app",
-//   "http://localhost:3000"]);
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   );
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', ["https://main--warm-cendol-deab82.netlify.app",
+  "http://localhost:3000"]);
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.use(
   session({
